ea5d5687e11432cc516b5fee719917bdd37c810e
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2019-04-13 23:23:32 -0300

    new items

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 
 //iniciando o app
 const app = express()
 app.use(express.json())
 app.use(express.static(__dirname + '/'))
+app.use(cors())
 
 //conectando o banco
 //mongoose.connect('mongodb://127.0.0.1:27017/nodeapi', {useNewUrlParser: true})
