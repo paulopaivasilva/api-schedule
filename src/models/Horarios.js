ed52426068ba146bf753f1b04f0d55a7c4b37e49
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2019-04-16 12:48:41 -0300

    Update Horarios.js

--- src/models/Horarios.js
+++ src/models/Horarios.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const HorarioSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
     date: {
         type: String,
         required: true
