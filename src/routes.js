fdc51b30a5780a10c5c6de154dfca43ab3236cde
Paulo Henrique <paulohenriquepaiva@ymail.com>
2019-12-20 15:00:37 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,5 @@ routes.get('/products', productController.index)
 routes.post('/products', productController.store)
 routes.get('/horarios', horarios.index)
 routes.post('/horarios', horarios.store)
-routes.post('/horarios/changeStatus', horarios.changeStatus)
+routes.post('/changeStatus', horarios.changeStatus)
 module.exports = routes
