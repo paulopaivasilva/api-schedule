8c72c077a8799898906e65bd4de873df371d88fd
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2020-12-07 09:28:47 -0300

    Mudanças

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,8 +5,5 @@ module.exports = {
     async index(req, res){
         const products = await Product.find()
         return res.json(products)
-    },
-    async store(req, res){
-
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,6 @@ const productController = require('./controllers/ProductController')
 const horarios = require('./controllers/HorarioController')
 
 routes.get('/products', productController.index)
-routes.post('/products', productController.store)
 routes.get('/horarios', horarios.index)
 routes.post('/horarios', horarios.store)
-routes.post('/changeStatus', horarios.changeStatus)
 module.exports = routes
